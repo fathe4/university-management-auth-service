1040843d783988a84f1bd58faa33cfb00e63dc97
f.karim <f.karim@pythys.com>
2023-05-30 11:20:52 +0600

    test

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function boostrap() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
     app.listen(config.port, () => {
-      console.log(`app listening on port ${config.port}`);
-    });
+      console.log(`app listening on port ${config.port}`)
+    })
 
-    console.log("Database connection Success");
+    console.log('Database connection Success')
   } catch (err) {
-    console.log("ERROR Connecting to db", err);
+    console.log('ERROR Connecting to db', err)
   }
 }
-boostrap();
+boostrap()
