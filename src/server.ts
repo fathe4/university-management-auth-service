df7f37acb320b5b763fdd6b96a438b68b61d6987
f.karim <f.karim@pythys.com>
2023-05-30 09:49:21 +0600

    [Done] setup mongoose env parser

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function boostrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`app listening on port ${config.port}`);
+    });
+
+    console.log("Database connection Success");
+  } catch (err) {
+    console.log("ERROR Connecting to db", err);
+  }
+}
+boostrap();
